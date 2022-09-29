23740cbe81d401083e32e33490a59277cbb79a54
taufik17 <taufikagungsantoso17@gmail.com>
2022-09-29 08:56:16 +0700

    [add] get data & tambah barang

---
+++ pages/api/transaksi.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export default function handler(req, res) {
+  axios
+    .get(`${process.env.BASE_URL}/transaksi`)
+    .then((response) => {
+      res.status(200).json(response?.data);
+    })
+    .catch((error) => {
+      res.status(400).json({ message: error?.response?.data?.message });
+    });
+}
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,56 @@
+import { useEffect, useState } from "react";
 import Head from "next/head";
 import Navbar from "../components/organisms/navbar";
 import { Table, Breadcrumb } from "react-bootstrap";
+import { BiEdit, BiTrash, BiPlusCircle } from "react-icons/bi";
+import axios from "axios";
+import moment from "moment";
+import Link from "next/link";
 
 export default function Home() {
+  const [barang, setBarang] = useState([]);
+  const [jenis, setJenis] = useState([]);
+  const [transaksi, setTransaksi] = useState([]);
+
+  useEffect(() => {
+    getBarang();
+    getJenis();
+    getTransaksi();
+  }, []);
+
+  const getBarang = () => {
+    axios
+      .get("/api/barang")
+      .then((res) => {
+        setBarang(res?.data?.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const getJenis = () => {
+    axios
+      .get("/api/jenis")
+      .then((res) => {
+        setJenis(res?.data?.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const getTransaksi = () => {
+    axios
+      .get("/api/transaksi")
+      .then((res) => {
+        setTransaksi(res?.data?.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <>
       <Head>
@@ -16,99 +64,116 @@ export default function Home() {
           <Breadcrumb.Item active>Home</Breadcrumb.Item>
         </Breadcrumb>
         <div className="row">
-          <div className="col">
+          <div className="col-8">
             <h5>Barang</h5>
+            <Link href="/tambahBarang" passHref>
+              <button type="button" class="btn btn-primary mb-2">
+                <BiPlusCircle /> Tambah Barang
+              </button>
+            </Link>
             <Table striped bordered hover>
               <thead>
                 <tr>
                   <th>No</th>
-                  <th>First Name</th>
-                  <th>Last Name</th>
-                  <th>Username</th>
+                  <th>Nama Barang</th>
+                  <th>Stok</th>
+                  <th>Jenis Barang</th>
+                  <th>Aksi</th>
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td>1</td>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                  <td>@mdo</td>
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td>Jacob</td>
-                  <td>Thornton</td>
-                  <td>@fat</td>
-                </tr>
-                <tr>
-                  <td>3</td>
-                  <td colSpan={2}>Larry the Bird</td>
-                  <td>@twitter</td>
-                </tr>
+                {barang.map((item, index) => (
+                  <>
+                    <tr>
+                      <td>{index + 1}</td>
+                      <td>{item.nama_barang}</td>
+                      <td>{item.stok}</td>
+                      <td>{item.nama_jenis}</td>
+                      <td>
+                        <button
+                          type="button"
+                          class="btn btn-danger btn-sm mx-1"
+                        >
+                          <BiTrash />
+                        </button>
+                        <button
+                          type="button"
+                          class="btn btn-warning btn-sm mx-1"
+                        >
+                          <BiEdit />
+                        </button>
+                      </td>
+                    </tr>
+                  </>
+                ))}
               </tbody>
             </Table>
           </div>
-          <div className="col">
+          <div className="col-4">
             <h5>Jenis</h5>
+            <button type="button" class="btn btn-primary mb-2">
+              <BiPlusCircle /> Tambah Jenis barang
+            </button>
             <Table striped bordered hover>
               <thead>
                 <tr>
-                  <th>#</th>
-                  <th>First Name</th>
-                  <th>Last Name</th>
-                  <th>Username</th>
+                  <th>No</th>
+                  <th>Nama Jenis</th>
+                  <th>Aksi</th>
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td>1</td>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                  <td>@mdo</td>
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td>Jacob</td>
-                  <td>Thornton</td>
-                  <td>@fat</td>
-                </tr>
-                <tr>
-                  <td>3</td>
-                  <td colSpan={2}>Larry the Bird</td>
-                  <td>@twitter</td>
-                </tr>
+                {jenis.map((item, index) => (
+                  <>
+                    <tr>
+                      <td>{index + 1}</td>
+                      <td>{item.nama_jenis}</td>
+                      <td>
+                        <button
+                          type="button"
+                          class="btn btn-danger btn-sm mx-1"
+                        >
+                          <BiTrash />
+                        </button>
+                        <button
+                          type="button"
+                          class="btn btn-warning btn-sm mx-1"
+                        >
+                          <BiEdit />
+                        </button>
+                      </td>
+                    </tr>
+                  </>
+                ))}
               </tbody>
             </Table>
           </div>
+        </div>
+        <div className="row my-4 justify-content-center">
           <div className="col">
             <h5>Transaksi</h5>
             <Table striped bordered hover>
               <thead>
                 <tr>
-                  <th>#</th>
-                  <th>First Name</th>
-                  <th>Last Name</th>
-                  <th>Username</th>
+                  <th>No</th>
+                  <th>Nama barang</th>
+                  <th>Jenis Barang</th>
+                  <th>Jumlah</th>
+                  <th>Tanggal</th>
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td>1</td>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                  <td>@mdo</td>
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td>Jacob</td>
-                  <td>Thornton</td>
-                  <td>@fat</td>
-                </tr>
-                <tr>
-                  <td>3</td>
-                  <td colSpan={2}>Larry the Bird</td>
-                  <td>@twitter</td>
-                </tr>
+                {transaksi.map((item, index) => (
+                  <>
+                    <tr>
+                      <td>{index + 1}</td>
+                      <td>{item.nama_barang}</td>
+                      <td>{item.nama_jenis}</td>
+                      <td>{item.jumlah}</td>
+                      <td>{moment(item.tgl_transaksi).format("ll")}</td>
+                    </tr>
+                  </>
+                ))}
               </tbody>
             </Table>
           </div>
