f686f5da7d08f8b6321ded42b23bfbf00291dc51
taufik17 <taufikagungsantoso17@gmail.com>
2022-09-30 11:03:28 +0700

    [fix] bug minor

---
+++ pages/api/simpanTransaksi.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+export default function handler(req, res) {
+  const { data } = req.body;
+
+  axios
+    .post(`${process.env.BASE_URL}/transaksi/tambah`, { data })
+    .then((response) => {
+        res.status(200).json(response?.data)
+    })
+    .catch((error) => {
+      res.status(400).json({ message: error?.response?.data?.message });
+    });
+}
