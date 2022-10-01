3fe859f45ffaa0e3507abfcb532a29f9044c5677
taufik17 <taufikagungsantoso17@gmail.com>
2022-10-01 15:04:57 +0700

    [add] modal transaksi

--- components/organisms/navbar.js
+++ components/organisms/navbar.js
@@ -3,24 +3,7 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 
 function NavbarLight() {
   return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Link href="/" passHref>
-          <Navbar.Brand href="#home">Penjualan</Navbar.Brand>
-        </Link>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Link href="/" passHref>
-              <Nav.Link>Data</Nav.Link>
-            </Link>
-            <Link href="/transaksi" passHref>
-              <Nav.Link>Transaksi</Nav.Link>
-            </Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <></>
   );
 }
 
