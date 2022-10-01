b35b9331be1b31ce4e626d819219d4bee7926185
taufik17 <taufikagungsantoso17@gmail.com>
2022-10-01 17:00:05 +0700

    [fix] env

--- pages/api/barang.js
+++ pages/api/barang.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default function handler(req, res) {
   axios
-    .get(`${process.env.BASE_URL}/barang`)
+    .get(`${process.env.NEXT_PUBLIC_BASE_URL}/barang`)
     .then((response) => {
       res.status(200).json(response?.data);
     })
